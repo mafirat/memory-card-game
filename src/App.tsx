e1bd404de960dd9f2562483ddfc297a010db53bd
mafirat <mafirat@hotmail.com>
2020-07-22 02:02:02 +0300

    v3 son

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { ToConsole } from './PrintFromJS';
-import { ToConsoleTS } from './PrintFromTS';
-interface IMyObject {
-  name: string,
-  lastName: string | null | number,
-  age?: number
-}
-
-function App() {
-  // let degisken2: string = "Mehmet";
-  // let degisken3: IMyObject;
-  // degisken3 = {
-  //   name: "Mehmet",
-  //   lastName: null,
-  // }
-  ToConsole("Mehmet")
-  ToConsole(29)
-  ToConsole({ isOk: false })
-
-  ToConsoleTS(20)
-  ToConsoleTS(5)
+import { Printer } from "./Printer"
+// import { PrinterJS } from './PrinterJS';
+const App = () => {
   return (
     <div className="App">
       Hello TypeScript
+      <hr />
+      <Printer name="Ali" age={22} isActive={true}  />
+      {/* <hr />
+      <PrinterJS  /> */}
     </div>
   );
 }
