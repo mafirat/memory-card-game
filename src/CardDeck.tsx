5b6a12d937803a1ccf6abd7c9dbd23f8ff8c1215
mafirat <mafirat@hotmail.com>
2020-07-22 02:58:52 +0300

    v6 son

--- src/CardDeck.tsx
+++ src/CardDeck.tsx
@@ -1,31 +1,27 @@
 import * as React from 'react';
 import { Card } from './components/Card';
 import { ICard } from './Types';
-const data: ICard[] = [
-    {
-        id: 1,
-        content: "A",
-        state: "unmatched"
-    },
-    {
-        id: 2,
-        content: "B",
-        state: "selected"
-    },
-    {
-        id: 3,
-        content: "B",
-        state: "matched"
-    },
-    {
-        id: 4,
-        content: "A",
-        state: "unmatched"
-    },
-]
-class CardDeck extends React.Component {
+import { data } from './data';
+interface IState {
+    cards: ICard[];
+}
+class CardDeck extends React.Component<{}, IState> {
+    selectedCardIds: number[] = [];
+    state: IState = {
+        cards: data
+    }
+    cardClickHandler = (card: ICard) => {
+        const { cards } = this.state;
+        if (this.selectedCardIds.length < 2) {
+            this.selectedCardIds.push(card.id);
+            this.setState({
+                ...this.state,
+                cards: cards.map(c => c.id === card.id ? card : c)
+            })
+        }
+    }
     render() {
-        const cardList = data.map(c => (<Card key={c.id} card={c} />))
+        const cardList = this.state.cards.map(c => (<Card key={c.id} card={c} clickHandler={this.cardClickHandler} />))
         return (
             <div className="container p-3 bg-dark">
                 <div className="card-columns" style={{ columnCount: 2 }}>
---
+++ src/data.ts
@@ -0,0 +1,24 @@
+import { ICard } from "./Types";
+
+export const data: ICard[] = [
+    {
+        id: 1,
+        content: "A",
+        state: "unmatched"
+    },
+    {
+        id: 2,
+        content: "B",
+        state: "unmatched"
+    },
+    {
+        id: 3,
+        content: "B",
+        state: "unmatched"
+    },
+    {
+        id: 4,
+        content: "A",
+        state: "unmatched"
+    },
+]
